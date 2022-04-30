4c357dac1fc9c2efe502c52ae0fc03d0183c7c8c
Jake Stopford <Jake_Stopford@hotmail.com>
2022-04-30 23:06:36 +0100

    added string ends with ? solution

---
+++ Codewars/String ends with question mark/Kata.cs
@@ -0,0 +1,15 @@
+﻿namespace Codewars.Remove_anchor_from_URL
+{
+    public static class StringEndsWithQuestionMarkKata
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (str.EndsWith(ending))
+            { 
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
